8784b256baef1fca5cd2476eb2ad72f7ab6ef03a
Marcus Davanco <md.daves@gmail.com>
2022-06-26 02:26:36 -0300

    feat: banner mobile adjustments

--- src/components/banner/styles.ts
+++ src/components/banner/styles.ts
@@ -18,10 +18,24 @@ export const Title = styled.p`
   font-family: "Barrio";
   font-size: 3rem;
   color: #fff;
+
+  @media (max-width: 720px) {
+    & {
+      text-align: center;
+    }
+  }
 `;
 
 export const Text = styled.p`
   font-weight: 400;
   font-size: 1.5rem;
   line-height: 2.063rem;
+
+  @media (max-width: 720px) {
+    & {
+      text-align: center;
+      max-width: 220px;
+      margin-top: 20px;
+    }
+  }
 `;
