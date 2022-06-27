091e6a6bd38baa702ca508173bcfedd397995616
Marcus Davanco <md.daves@gmail.com>
2022-06-27 06:40:39 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:marcusdavanco/marcusdavanco.github.io.git main
+git push -f main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
