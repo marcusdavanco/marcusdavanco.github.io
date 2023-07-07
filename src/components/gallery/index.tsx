c78c04d155128ac7cae30423d9d7cce60e52031e
Marcus Davanço <md.daves@gmail.com>
2023-07-07 16:43:55 -0300

    feat: gallery update

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -7,11 +7,11 @@ export const Gallery = () => {
       <h2>My latest work</h2>
       <Content>
         <Card href="https://github.com/marcusdavanco/bookwise" imageUrl="https://github.com/marcusdavanco/bookwise/raw/main/.github/bookwise.png" />
+        <Card href="https://github.com/marcusdavanco/design-system" imageUrl="https://github.com/marcusdavanco/design-system/raw/main/.github/design_system.png" />
         <Card href="https://github.com/marcusdavanco/ignite-shop" imageUrl="https://github.com/marcusdavanco/ignite-shop/raw/main/.github/ignite-shop.png" />
         <Card href="https://github.com/marcusdavanco/github-blog" target="_blank" imageUrl="https://github.com/marcusdavanco/github-blog/raw/main/.github/github-blog.png" animate />
         <Card href="https://github.com/marcusdavanco/coffe-delivery" target="_blank" imageUrl="https://github.com/marcusdavanco/coffe-delivery/raw/main/.github/coffe-delivery.png" animate />
         <Card href="https://github.com/marcusdavanco/todo-list" target="_blank" imageUrl="https://github.com/marcusdavanco/todo-list/raw/main/.github/preview_web.png" />
-        <Card href="#" imageUrl="" />
         <Button
           activeColor="#fff"
           color="#fff"
