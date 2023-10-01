310ee2417a2b1dfc4425949c5e48c8ddecc780a1
Marcus Davanço <md.daves@gmail.com>
2023-09-30 22:52:06 -0300

    feat: included dribble link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,6 +6,8 @@ import Github from "../../assets/github.svg?component";
 import Linkedin from "../../assets/linkedin.svg?component";
 // @ts-ignore
 import Email from "../../assets/email.svg?component";
+// @ts-ignore
+import Dribbble from "../../assets/dribbble.svg?component";
 
 export const Footer = () => {
   return (
@@ -15,12 +17,16 @@ export const Footer = () => {
           <a href="https://github.com/marcusdavanco" target="_blank">
             <Github />
           </a>
+          <a href="https://dribbble.com/marcusdavanco" target="_blank">
+            <Dribbble />
+          </a>
           <a
             href="https://www.linkedin.com/in/marcusdavanco/"
             target="_blank"
           >
             <Linkedin />
           </a>
+         
           <a href="mailto:marcusdavanco@gmail.com" target="_blank">
             <Email />
           </a>
